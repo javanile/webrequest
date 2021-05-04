e038f418061b0448fbbd726673162ec71669a6c9
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-05-04 02:47:38 +0200

    first commit

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ $sources = [
 ];
 
 $_SERVER['REQUEST_URI'];
+$file = tempnam(sys_get_temp_dir(), 'FOO.php');
+$url = 'https://raw.githubusercontent.com/javanile/webrequest/main/tests/fixtures/script.php';
+$script = file_get_contents($url);
+file_put_contents($file, $script);
 
-
-
+require_once $file;
