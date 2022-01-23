ad76b9ec00190d43be92b629f91f3b015384760a
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-23 19:17:52 +0100

    Add exception to handle Spreadsheet clients

--- public/index.php
+++ public/index.php
@@ -1,6 +1,28 @@
 <?php
 /**
+ * Webrequest
  *
+ * Webhooks Adapter suitable for 99% of No-code scenarios.
+ *
+ * Copyright (c) 2020 Francesco Bianco <bianco@javanile.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -17,7 +39,7 @@ $variantPath = $vendor.'/'.$package.($variant ? '/'.$variant : '');
 $publicUrl = 'http://'.$_SERVER['HTTP_HOST'].'/'.$variantPath.'?';
 $repository = $vendor.'/'.$package;
 $platform = 'github';
-$bypassLandingPage = isset($_GET['_bypass_landing_page']) && $_GET['_bypass_landing_page'] == 'yes';
+$bypassLandingPage = str_contains($_SERVER['HTTP_USER_AGENT'], 'spreadsheets') || (isset($_GET['_bypass_landing_page']) && $_GET['_bypass_landing_page'] == 'yes');
 $isRequest = ($_SERVER['REQUEST_METHOD'] == 'POST') || $bypassLandingPage;
 $controllerUrl = 'https://raw.githubusercontent.com/'.$vendor.'/'.$package.'/main/webrequest'.($variant ? '-'.$variant : '').'.php';
 $controllerErrorUrl = 'https://raw.githubusercontent.com/javanile/webrequest/main/webrequest-error.php';
