d70ce2fcb664221ca0093dcbbea4da4219141032
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-29 13:35:21 +0100

    Update webrequest.php

--- webrequest.php
+++ webrequest.php
@@ -1,21 +1,19 @@
-<?php  /*..._             _   _            _
-      .'     '.      _   | | | | ___ _   _| |
-     /    .-""-\   _/ \  | |_| |/ _ \ | | | |
-   .-|   /:.   |  |   |  |  _  |  __/ |_| |_|
-   |  \  |:.   /.-'-./   |_| |_|\___|\__, (_)
-   | .-'-;:__.'    =/                |___/
-   .'=  *=|     _.='
-  /   _.  |    ;         Welcome to "webrequest.cc"
- ;-.-'|    \   |
-/   | \    _\  _\        << Few lines of code from you,
-\__/'._;.  ==' ==\            a giant leap for your project >>
-         \    \   |
-         /    /   /      Run small snippets without hosting
-         /-._/-._/       to accomplish complex task
-  jgs    \   `\  \       with No-code mindset
-          `-._/.*/
-
-/*\
+<?php /* _..._             _   _            _
+       .'     '.      _   | | | | ___ _   _| |
+      /    .-""-\   _/ \  | |_| |/ _ \ | | | |
+    .-|   /:.   |  |   |  |  _  |  __/ |_| |_|
+    |  \  |:.   /.-'-./   |_| |_|\___|\__, (_)
+    | .-'-;:__.'    =/                |___/
+    .'=  *=|     _.='
+   /   _.  |    ;         Welcome to "webrequest.cc"
+  ;-.-'|    \   |
+ /   | \    _\  _\        << Few lines of code from you,
+ \__/'._;.  ==' ==\            a giant leap for your project >>
+          \    \   |
+          /    /   /      Run small snippets without hosting
+          /-._/-._/       to accomplish complex task
+   jgs    \   `\  \       with No-code mindset
+           `-._/._/
 
 ## Get Started
 
@@ -44,7 +42,7 @@ You can use Webrequest.cc as layer of interconnaction between systems, as exampl
 - Remove or reduce large inputs into smaller one
 - Fork a signle No-code trigger to start multiple workflows
 
-\*/
+*/
 
 // This script just say Hello!
 echo "Hello, World!";
