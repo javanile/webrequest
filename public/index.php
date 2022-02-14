5addf9e7d0d78d03c6f0651193c3e1ec2e7b06b5
Francesco Bianco <info.francescobianco@gmail.com>
2022-02-14 12:50:45 +0100

    introduicing webrequest form

--- public/index.php
+++ public/index.php
@@ -27,6 +27,16 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -89,6 +99,8 @@ if (!file_exists($insightsFile) || filemtime($insightsFile) < $expireTime) {
 }
 
 if ($isRequest) {
+    header('Access-Control-Allow-Origin: *');
+
     // Update stats
     $currentMinute = intdiv(time(), 60);
     $delta = @$stats['minute'] > 0 ? $currentMinute - @$stats['minute'] : 0;
