629f9214101a34697a61a7f09ff6e7ee04d4618a
Francesco Bianco <info.francescobianco@gmail.com>
2022-02-14 15:32:00 +0100

    introduicing webrequest form

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Allow-Headers: *');
     header('Access-Control-Max-Age: 1728000');
     header('Content-Length: 0');
     header('Content-Type: text/plain');
